0ac81cee251b274eb0c7543e6d4d62072499dde0
Saif <saifsadiq.ss@gmail.com>
2021-01-06 14:36:14 +0500

    Navigation component skeleton added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 import Card from './components/Card/Card';
+import NavigationItems from './components/Navigation/NavigationItems/NavigationItems';
 
 function App() {
   return (
     <div className="App">
+      <NavigationItems />
       <h1>Hi</h1>
       <Card />
       <Card />
---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './NavigationItem.module.css';
+
+const NavigationItem = (props) => (
+    <li className={classes.NavigationItem}>NavigationItem</li>
+)
+
+export default NavigationItem;
