eb187504393977343eefbb04b5a0a1780c259b38
Saif <saifsadiq.ss@gmail.com>
2021-01-06 14:02:16 +0500

    Card component skeleton added

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Card.module.css';
+
+const Card = (props) => (
+    <div className={classes.Card}>
+        <a href='#'>img goes here</a>
+        <div className="card-body">
+        <h4 className="card-title">Salt Lamp</h4>
+            <p>Radiate a beautiful warm pink-reddish glow</p>
+        </div>
+    </div>
+)
+
+
+export default Card;
